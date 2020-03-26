f5c39ff28a6cec9f4d6949c629f957e24b00ae9e
wajeehamanzoor <wajeehamanzoor97@gmail.com>
2020-03-26 14:08:48 +0500

    VG-3 remaining

--- Team.py
+++ Team.py
@@ -1,9 +1,6 @@
 class Team:
 
-<<<<<<< HEAD
     #class attribute
-=======
->>>>>>> ce90cc6a1f50bae9820b232b5390bd198f36e464
     fee = 1000
     # Initializer / Instance Attributes
     def __init__(self, name, city, is_paid, num_players):
@@ -11,49 +8,3 @@ class Team:
         self.city = city
         self.is_paid = is_paid
         self.num_players = num_players
-<<<<<<< HEAD
-=======
-
-    # @property
-    # def name(self):
-    #     print("getter method called")
-    #     return self._name
-    #
-    #     # a setter function
-    #
-    # @name.setter
-    # def name(self, a):
-    #     self._name = a
-    #
-    # @property
-    # def city(self):
-    #     return self.city
-    #
-    #     # a setter function
-    #
-    # @city.setter
-    # def city(self, a):
-    #     self._city = a
-    #
-    # @property
-    # def isPaid(self):
-    #     return self._isPaid
-    #
-    #     # a setter function
-    #
-    # @isPaid.setter
-    # def isPaid(self, a):
-    #     self._isPaid = a
-    #
-    # @property
-    # def noOfPlayers(self):
-    #     return self._noOfPlayers
-    #
-    #     # a setter function
-    #
-    # @noOfPlayers.setter
-    # def noOfPlayers(self, a):
-    #     self._noOfPlayers = a
-    #
-    #
->>>>>>> ce90cc6a1f50bae9820b232b5390bd198f36e464
